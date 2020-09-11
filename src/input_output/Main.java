dbc5daad862bf6a6e7bb28adcc6287d8983d020f
Lampard <wjddns59@naver.com>
2020-09-11 14:21:43 +0900

    commit

---
+++ src/input_output/Main.java
@@ -0,0 +1,10 @@
+package input_output;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	} // main
+
+} // class
